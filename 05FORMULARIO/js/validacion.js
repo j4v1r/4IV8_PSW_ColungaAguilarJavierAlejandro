e9d8d42c363d092de8be49ffa440306a8874b05f
j4v1r <jcolungaa2000@alumno.ipn.mx>
2022-03-04 23:52:14 -0600

    Examen

--- 05FORMULARIO/js/validacion.js
+++ 05FORMULARIO/js/validacion.js
@@ -16,7 +16,7 @@ CONST
 
 */
 
-function validar(formulario){
+/*function validar(formulario){
 
     if(formulario.nombre.value.lenght < 5){
         alert("Escriba por lo menos cinco caracteres para el nombre");
@@ -79,7 +79,7 @@ function validar(formulario){
     para poder haber uso de uan expresión regular
     */
 
-    var txt=formulario.email.value;
+    /*var txt=formulario.email.value;
     
     //Crear expresión
 
@@ -89,4 +89,75 @@ function validar(formulario){
 
     return b.test;
 
+}*/
+
+function validar(formulario){
+    
+    if(formulario.nombre.value.length < 3){
+        alert("Escriba por lo menos  5 caracteres para el nombre");
+        formulario.nombre.focus();
+        return false();
+    }
+
+    var checarOK = "QWERTYUIOIPASDFGHJKLÑZXCVBNM" + "qwertyuiopasdfghjklñzxcvbnm";
+
+    var checarStr = formulario.nombre.value;
+
+    var allValido = true;
+
+    for(var i =0; i < checarStr; i++){
+        var ch= checarStr.charAt(i)
+        for(var j = 0; j < checarOK.length; j++){
+            if(ch == checarOK.charAt(j))
+            break;
+        }
+        if(j == checarOK.length){
+            allValido = false;
+            break;
+        }
+    }
+
+    if(allValido){
+        alert("Escriba unicamente letras en el campo de nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    var checarOK = "0123456789";
+    var checarStr = formulario.edad.value;
+
+    var allValido = true;
+
+    for(var i =0; i < checarStr; i++){
+        var ch= checarStr.charAt(i)
+        for(var j = 0; j < checarOK.length; j++){
+            if(ch == checarOK.charAt(j))
+            break;
+        }
+        if(j == checarOK.length){
+            allValido = false;
+            break;
+        }
+    }
+
+    if(allValido){
+        alert("Escriba unicamente letras en el campo de nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    /*
+    Es necesario que busquen como crear expresiones regulares
+    para poder haber uso de una  expresion regular
+    */
+
+    var txt = formulario.email.value;
+
+    //crear expresion
+
+    var b = /^[^@\s]+[^@\.\s]+(\.[^@\.\s]+)+$/;
+
+    alert("Email " + (b.test(txt)?" ":"no")+"valido");
+
+    return b.test;
 }
--- EXAMEN 1P/js/validacion.js
+++ EXAMEN 1P/js/validacion.js
@@ -2,16 +2,17 @@
 máximo de 1,000,000*/
 
 
-function validar(examen){
+function validar(formulario){
 
-    if(formulario.valor.value > 1000000){
-        alert("El valor del automóvil debe ser mínimo de 100,00 y máximo de 1,000,000");
-        formulario.nombre.focus();
+    if(formulario.valorau.value < 100000 || formulario.valorau.value >1000000){
+        alert("El valor del auto debe estar entre 100,000 y 1,000,000");
+        formulario.valorau.focus();
         return false;
     }
 
     var checkOK="0123456789";
-    var checkStr=formulario.valor.value;
+
+    var checkStr=formulario.valorau.value;
 
     var allValido=true;
 
@@ -26,3 +27,51 @@ function validar(examen){
             break;
         }
     }
+
+    if(!allValido){
+        alert("Escriba unicamente letras en el campo de nombre");
+        formulario.valorau.focus();
+        return false;
+    }
+
+    
+
+    var checkOK="0123456789";
+    var checkStr=formulario.edad.value;
+
+    var allValido=true;
+
+    for(var i=0;i<checkStr; i++){
+        var ch=checkStr.charAt(i)
+        for(var j=0;j<checkOK.length;j++){
+            if(ch==checkOK.charAt(j))
+            break;
+        }
+        if(j==checkOK.length){
+            allValido=false;
+            break
+        }
+    }
+
+    if(allValido){
+        alert("Escriba uncamente letras en el campo de nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    /*
+    Es necesario que busquen cmo crear expresiones regulares
+    para poder haber uso de uan expresión regular
+    */
+
+    var txt=formulario.email.value;
+    
+    //Crear expresión
+
+    var b = /^[^@s]+^+(.)+/;
+
+    alert("Email" + (b.test(txt)?" ":"no")+"valido");
+
+    return b.test;
+
+}
