8fead9ebd34a86fd2ee7f1b928a847f3b3e0b6c7
j4v1r <jcolungaa2000@alumno.ipn.mx>
2022-03-12 10:59:01 -0600

    JS

--- 06JAVASCRIPT/JS/interes.js
+++ 06JAVASCRIPT/JS/interes.js
@@ -1,12 +1,38 @@
-function validar(e){
+//Función que se encargue de validar por expresión regular
 
-    teclado= (document.all)?e.keyCode: e.which;
-    /*Vamos a aplicar una prueba lógica a todo el teclado a partir de un patrón, este
-    patrón debe ser númerico y solo acepta del 0 a 9. Todo lo demás lo deja escapar*/
+function validarn(e){
+
+    var teclado = (document.all)?e.keyCode : e.which;
+    /*Vamos a aplicar una prueba lógica a todo el teclado a partir 
+    de un patrón, este patrón debe ser númerico y solo acepta del 
+    0 a 9. Todo lo demás lo deja escapar*/
     if (teclado==8) return true;
-    var patrón= /[0_9\d .]/;
+    var patron= /[0-9\d .]/;
 
     var probar = String.fromCharCode(teclado);
-    return probar.test(probar);
+
+    return patron.test(probar);
+
+}
+
+function interes(){
+
+    var valor = document.formulario1.cantidad.value;
+
+    //Tipo de dato
+
+    var resul = parseInt(valor);
+
+    var interes = resul*0.025;
+
+    var total = resul+interes;
+
+    document.formulario1.sueldoI.value = "$" + total;
+}
+
+function borrar(){
+
+    document.formulario1.sueldoI.value = "";
+    document.formulario1.cantidad.value = "";
 
 }
