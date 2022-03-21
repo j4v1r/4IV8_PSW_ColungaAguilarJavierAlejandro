23cdc6502ea4a424e802438db54f7f88a8e7e1d2
j4v1r <jcolungaa2000@alumno.ipn.mx>
2022-03-21 16:53:31 -0600

    Ejercicio 2

--- 07ALGORITMIAJS/js/logica.js
+++ 07ALGORITMIAJS/js/logica.js
@@ -22,6 +22,41 @@ function problema1n(){
 
 function problema2n(){
 
+   var tx1 = document.getElementById("p2-x1").value;
+   var tx2 = document.getElementById("p2-x2").value;
+   var tx3 = document.getElementById("p2-x3").value;
+   var tx4 = document.getElementById("p2-x4").value;
+   var tx5 = document.getElementById("p2-x5").value;
+   var ty1 = document.getElementById("p2-y1").value;
+   var ty2 = document.getElementById("p2-y2").value;
+   var ty3 = document.getElementById("p2-y3").value;
+   var ty4 = document.getElementById("p2-y4").value;
+   var ty5 = document.getElementById("p2-y5").value;
+
+   var x1 = parseFloat(tx1);
+   var x2 = parseFloat(tx2);
+   var x3 = parseFloat(tx3);
+   var x4 = parseFloat(tx4);
+   var x5 = parseFloat(tx5);
+   var y1 = parseFloat(ty1);
+   var y2 = parseFloat(ty2);
+   var y3 = parseFloat(ty3);
+   var y4 = parseFloat(ty4);
+   var y5 = parseFloat(ty5);
+
+   if(tx1=="" || tx2=="" || tx3=="" || tx4=="" || tx5=="" ||
+   ty1=="" || ty2=="" || ty3=="" || ty4=="" || ty5==""){
+
+       alert("Introduzca un valor de vector");
+
+   }else{
+
+    var producto = (x1*y1) + (x2*y2) + (x3*y3) + (x4*y4) + (x5*y5);
+ 
+    document.getElementById('p2-output').textContent = "El producto escalar es " + producto;
+
+   }
+
 }
 
 
